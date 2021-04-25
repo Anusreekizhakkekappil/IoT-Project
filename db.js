08db3195e18ba56115a1ea112d516cfd0545ecdc
Anusreekizhakkekappil <anusreekk07@gmail.com>
2021-04-25 14:21:11 +1000

    commit

--- db.js
+++ db.js
@@ -12,36 +12,28 @@ async function main() {
 
 //insert data
 
-MongoClient.connect(url, function(err, db) {
-            if (err) throw err;
-            var dbo = db.db("patient_details");
-            var myobj = { name: "sam", address: "diabetes" };
-            dbo.collection("patient").insertOne(myobj, function(err, res) {
-                if (err) throw err;
-                console.log("1 document inserted");
-                db.close();
-            });
 
-            async function findListings(client, resultsLimit) {
-                const cursor = client
-                    .db('patient_details')
-                    .collection('listingsAndReviews')
-                    .find()
-                    .limit(resultsLimit);
 
-                const results = await cursor.toArray();
-                if (results.length > 0) {
-                    console.log(`Found ${results.length} listing(s):`);
-                    results.forEach((result, i) => {
-                        date = new Date(result.last_review).toDateString();
+async function findListings(client, resultsLimit) {
+    const cursor = client
+        .db('patient_details')
+        .collection('listingsAndReviews')
+        .find()
+        .limit(resultsLimit);
 
-                        console.log();
-                        console.log('connected');
-                        console.log(
-                            `   most recent review date: ${new Date(
+    const results = await cursor.toArray();
+    if (results.length > 0) {
+        console.log(`Found ${results.length} listing(s):`);
+        results.forEach((result, i) => {
+            date = new Date(result.last_review).toDateString();
+
+            console.log();
+            console.log('connected');
+            console.log(
+                `   most recent review date: ${new Date(
             result.last_review
           ).toDateString()}`
-                        );
-                    });
-                }
-            }
+            );
+        });
+    }
+}
